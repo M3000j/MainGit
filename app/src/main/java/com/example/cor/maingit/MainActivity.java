347f1c61e46c252f692f2c5c506eac6e0426da27
M3000 <mohammadjavadian71@yahoo.com>
2017-05-18 11:53:48 +0430

    First Commit

---
+++ app/src/main/java/com/example/cor/maingit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.cor.maingit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Integer a;
+        Integer b;
+        int g;
+        String re;
+        String we;
+    }
+}
